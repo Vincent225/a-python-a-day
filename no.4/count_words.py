08bf2545998a7e101ac8c3784c72f9201fcd3788
michaelyou <michaelseu2011@gmail.com>
2015-04-02 17:16:06 -0400

    file iter

--- no.4/count_words.py
+++ no.4/count_words.py
@@ -7,17 +7,12 @@ import re
 def count_word(file_name):
     f = open(file_name)
     
-    line = f.readline()
     word_counter = collections.Counter()
 
-    while line:
-        #找出所有单词，保存为数组，lower表示将整句话变为小写
+    #文件对象使用迭代器
+    for line in f:
         words = re.findall('\w+', line.lower())
-
         word_counter.update(words)
-
-        #读取下一行
-        line = f.readline()
 
     f.close()
 
